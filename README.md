3ad7b16dca0d76ceeadb2dfb604e9b7dafa6707b
Lucas Vicente da Silva <78667161+Lucasvicentedasilva@users.noreply.github.com>
2024-07-08 18:29:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my-todo-fullstack
+ 
---
+++ server/api/todos/[id].js
@@ -0,0 +1,16 @@
+export default eventHandler(async (event) => {
+    // Get the ID from the event (assuming it's passed as an argument)
+    const { id } = event.context.params
+  
+    if (!id) {
+      throw new Error('Missing ID to delete todo');
+    }
+  
+    const db = hubDatabase();
+  
+    // Delete the todo with the specific ID
+    await db.prepare('DELETE FROM todos WHERE id = ?').bind(id).run();
+  
+    // No need to return anything as the todo is deleted
+  
+  });
